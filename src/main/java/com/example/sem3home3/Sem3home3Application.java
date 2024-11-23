234e893d062beecae14848ce0101a56fa6d81d15
silver_kir <rci@mp03.ru>
2024-11-23 21:11:39 +0800

    initial commit

---
+++ src/main/java/com/example/sem3home3/Sem3home3Application.java
@@ -0,0 +1,13 @@
+package com.example.sem3home3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem3home3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem3home3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sem3home3/Sem3home3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sem3home3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem3home3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
